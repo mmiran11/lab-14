9f5af8af457e47ad8a151e86689f7398224a9b6a
mmiran11 <mmiran11@J15623.win.kennesaw.edu>
2019-11-22 12:07:12 -0500

    defined more methods.

--- programs/LinearBinarySearch.java
+++ programs/LinearBinarySearch.java
@@ -1,10 +1,29 @@
 package programs;
 
+import java.util.Scanner;
 public class LinearBinarySearch {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		//part 1
+		int[] myArray = new int [5];
+		for (int i = 0; i < myArray.length; i++)
+		{
+			System.out.print ("Enter a number: ");
+			myArray[i] = scan.nextInt();
+		}
 		
-
+		for (int i = 0; i < myArray.length; i++)
+		{
+			System.out.print (myAray[i] + " | ");
+		}
+		
+		System.out.println();
+		int compCount = LinearSearch(myArray, 5);
+		System.out.println("Found 5 after"  + compCount + " comparisons");
+		int binCompCount5 = BinarySearch(myArray, 5);
+		System.out.println("Found 1 after " + binCompCount5 + " comparisons");
 	}
 	
 	public static int LinearSearch(int[] someArray, int target)
@@ -22,7 +41,30 @@ public class LinearBinarySearch {
 	
 	public static int BinarySearch(int[] someArray, int target)
 	{
-		
+		int comparisons = 0;
+		int highIndex = someArray.length;
+		int lowIndex = 0;
+		int midIndex = (highIndex + lowIndex) / 2;
+		boolean found = false;
+		while (!found) 
+		{
+			comparisons++;
+			if(someArray[midIndex] == target)
+			{
+				found = true;
+			}
+			else if (someArray[midIndex] > target)
+			{
+				highIndex = midIndex;
+				
+			}
+			else if (someArray[midIndex] < target) 
+			{
+				lowIndex = midIndex;
+			}
+			midIndex = (highIndex + lowIndex / 2);
+		}
+		return comparisons;
 	}
 	
 	public static int BubbleSort(int[] someArray) //for exercise 2
@@ -43,6 +85,7 @@ public class LinearBinarySearch {
 				comparisons++;
 			}
 		}
+		return swaps;
 	}
 
 }
